b42101e04199d4f45f2d0ce2302bb229b24735c5
MusawerAli <pakjalihouse@gmail.com>
2019-08-21 17:50:50 +0500

    sql update

--- db/society_db.sql
+++ db/society_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 20, 2019 at 04:03 PM
+-- Generation Time: Aug 21, 2019 at 05:50 PM
 -- Server version: 5.7.27-0ubuntu0.18.04.1
--- PHP Version: 7.2.19-0ubuntu0.18.04.2
+-- PHP Version: 7.2.19-0ubuntu0.18.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -188,8 +188,8 @@ CREATE TABLE `login` (
   `name` varchar(30) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `type` varchar(15) DEFAULT NULL,
-  `status` varchar(10) DEFAULT 'blocked'
+  `type` enum('','society_officer','member','admin') DEFAULT '',
+  `status` enum('Active','blocked') DEFAULT 'blocked'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -197,13 +197,13 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`login_id`, `name`, `password`, `email`, `type`, `status`) VALUES
-(1, 'Nauman', '1234', 'nauman@gmail.com', 'society_officer', 'Active'),
-(12, 'Saima', '1234', 'saima@gmail.com', 'admin', 'Active'),
+(1, 'Saima', '1234', 'saima@gmail.com', 'admin', 'Active'),
 (13, 'Member', '1234', 'member@gmail.com', 'member', 'Active'),
 (19, '', '', '', '', 'Active'),
-(20, 'Musawer Ali', 'admin123', 'liaquat31202@gmail.com', 'member', 'Active'),
-(21, 'Musawer Ali', 'aliraza123', 'aliraza.k2@gmail.com', 'member', 'Active'),
-(22, 'Musawer Ali', 'adminadmin', 'admin@admin.com', 'member', 'blocked');
+(22, 'Musawer Ali', 'adminadmin', 'admin@admin.com', 'member', 'Active'),
+(28, 'Ch Musawer', 'admin123', 'liaquat31202@gmail.com', 'member', 'Active'),
+(29, 'Ghazanfar Ali', '1234', 'ghazanfar@gmail.com', 'society_officer', 'Active'),
+(30, 'Ghazanfar Ali', '123', 'musawer79@ovi.com', 'society_officer', 'Active');
 
 -- --------------------------------------------------------
 
@@ -272,9 +272,19 @@ CREATE TABLE `property_detail` (
   `plot_no` varchar(100) NOT NULL,
   `property_city` varchar(100) NOT NULL,
   `property_location` varchar(100) NOT NULL,
-  `transfer_login_ids` varchar(200) NOT NULL,
-  `login_cnic` varchar(200) NOT NULL
+  `transfer_login_ids` varchar(200) DEFAULT NULL,
+  `login_cnic` varchar(200) DEFAULT NULL,
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `property_detail`
+--
+
+INSERT INTO `property_detail` (`id`, `login_id`, `purpose`, `property_type`, `property_title`, `property_unit`, `unit_qty`, `price`, `property_desc`, `plot_no`, `property_city`, `property_location`, `transfer_login_ids`, `login_cnic`, `image`) VALUES
+(5, 21, 'rent', 'home', 'Commersial Plaza', 'acer', 54, 2323, '43sd', '32323', 'Bahawalpur', '1A,Avenue', NULL, NULL, 'partner-APW-AMG1076-hd.jpg'),
+(6, 28, 'rent', 'plot', 'Corner Plot', 'kanal', 4, 4000000, 'Alama iqbal town', '43alm', 'Lahore', '1A,Avenue', NULL, NULL, 'imj.jpg'),
+(7, 29, 'sale', 'home', 'Full furnished house', 'kanal', 5, 5000000, 'Fully fuenished with swimming pool and grossy', '54nml', 'Karachi', 'DHA phase 2', NULL, NULL, 'c23d57aa-4e3d-4c60-b522-b0460407170b.jpg');
 
 -- --------------------------------------------------------
 
@@ -312,7 +322,7 @@ CREATE TABLE `users_details` (
   `login_id` int(11) DEFAULT NULL,
   `cnic` varchar(250) DEFAULT NULL,
   `address` varchar(250) DEFAULT NULL,
-  `mobile_number` varchar(12) DEFAULT NULL,
+  `mobile_number` varchar(20) DEFAULT NULL,
   `image` varchar(345) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -321,16 +331,18 @@ CREATE TABLE `users_details` (
 --
 
 INSERT INTO `users_details` (`id`, `login_id`, `cnic`, `address`, `mobile_number`, `image`) VALUES
-(1, 1, '123456789', '195 f', '031388888', 'eee'),
-(2, 12, '66565555465456', 'tet', '6666', 'hhh'),
+(1, 11, '123456789', '195 f', '031388888', 'eee'),
+(2, 1, '66565555465456', 'tet', '6666', 'hhh'),
 (3, 13, '234245425', '234', '234324', '234'),
 (4, NULL, 'cnic', 'address', 'contact', ''),
 (5, 3, 'cnic', 'address', 'contact', ''),
 (6, 17, '4324242', 'H/s no 229 khan colony multan road near shama canima bahawalpur', '4234234', ''),
 (7, 19, '', '', '', ''),
-(8, 20, '765334534', 'H/s no 229 khan colony multan road near shama canima bahawalpur', '4234234', ''),
 (9, 21, '534534534', 'H/s no 229 khan colony multan road near shama canima bahawalpur', '53453453453', ''),
-(10, 22, '432424234234', 'H/s no 229 khan colony multan road near shama canima bahawalpur', '4234234234', '');
+(10, 22, '432424234234', 'H/s no 229 khan colony multan road near shama canima bahawalpur', '4234234234', ''),
+(11, 28, '323232', 'pak jali house, Near Shama Canima Multan Road Bahawalpur', '+923336513516', ''),
+(12, 29, '3120002343214', 'pak jali house, Near Shama Canima Multan Road Bahawalpur', '+923336513516', ''),
+(13, 30, '53211234', 'pak jali house, Near Shama Canima Multan Road Bahawalpur', '+923336513516', '');
 
 --
 -- Indexes for dumped tables
@@ -435,7 +447,7 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT for table `order_product`
 --
@@ -450,7 +462,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `property_detail`
 --
 ALTER TABLE `property_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `receipt`
 --
@@ -460,7 +472,7 @@ ALTER TABLE `receipt`
 -- AUTO_INCREMENT for table `users_details`
 --
 ALTER TABLE `users_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
