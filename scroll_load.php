a05c7b7e91a606be4455302218d3de763911af28
MusawerAli <pakjalihouse@gmail.com>
2019-08-22 15:21:31 +0500

    phase 2  complete

--- scroll_load.php
+++ scroll_load.php
@@ -4,7 +4,7 @@ require_once 'db.php';
 if(isset($_POST["limit"], $_POST["start"]))
 {
 
- $query = "SELECT * FROM property_detail ORDER BY id DESC LIMIT ".$_POST["start"].", ".$_POST["limit"]."";
+ $query = "SELECT * FROM property_detail INNER JOIN login ON login.login_id = property_detail.login_id ORDER BY property_detail.id DESC LIMIT ".$_POST["start"].", ".$_POST["limit"]."";
  $result = mysqli_query($conn, $query);
  while($row = mysqli_fetch_array($result))
  {
@@ -12,18 +12,24 @@ if(isset($_POST["limit"], $_POST["start"]))
  
  <!-- Blog entry -->
   <div class="w3-container w3-card w3-white w3-round w3-margin"><br>
-        <img src="http://localhost/housing/assets/dataimg/'.$row['image'].'" alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
-        <span class="w3-right w3-opacity">32 min</span>
-        <h4>Angie Jane</h4><br>
+        
+         <span class="w3-right w3-tag w3-round black w3-border w3-border-orange"><span class="fa fa-star-half-o w3-text-orange"></span> For '.$row['purpose'].'</span>
+         <br>
         <hr class="w3-clear">
-        <p>Have you seen this?</p>
+        <h2 class="w3-wide w3-text-orange">Fertile Land</h2>
+        <h3 class="w3-opacity">'.$row['unit_qty'].' ' .$row['property_unit'].'</h3>
+         <span class="w3-right w3-tag w3-round w3-white w3-border w3-border-green">(pkr) '.$row['price'].' /=</span>
+        <h5 class="w3-opacity"> Property no# '.$row['plot_no'].'</h5>
+        <h5>'.$row['property_location'].', ' .$row['property_city'].'</h5>
+
         <img src="http://localhost/housing/assets/dataimg/'.$row['image'].'" style="width:100%" class="w3-margin-bottom">
         <p>"'.$row['property_desc'].'"</p>
-        <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"><i class="fa fa-thumbs-up"></i>  Like</button> 
-        <button type="button" class="w3-button w3-theme-d2 w3-margin-bottom"><i class="fa fa-comment"></i>  Comment</button> 
+        <hr class="w3-clear">
+        <h4> '.$row['name'].'</h4> <strong>'.$row['email'].'</strong>
+       
       </div> 
   <hr>
-  <hr />
+  
   ';
  }
 }
